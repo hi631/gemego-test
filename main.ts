522cc74132d3543a2eff2fc7e5b8fdeb8d6286c5
hi631 <hi631@ac.auone-net.jp>
2026-02-20 09:25:09 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 let rmode = 0
 forever(function () {
-    rmode = 0
-    if (rmode) {
-        pins.LED.digitalWrite(true)
-    }
-    pause(200)
-    if (rmode) {
-        pins.LED.digitalWrite(false)
-    }
-    pause(200)
-})
-forever(function () {
     scene.setBackgroundImage(assets.image`autumn`)
     pause(1000)
     scene.setBackgroundImage(assets.image`forest2`)
     pause(1000)
+})
+forever(function () {
+    rmode = 1
+    if (rmode) {
+        pins.P13.digitalWrite(true)
+    }
+    pause(200)
+    if (rmode) {
+        pins.P13.digitalWrite(false)
+    }
+    pause(200)
 })
